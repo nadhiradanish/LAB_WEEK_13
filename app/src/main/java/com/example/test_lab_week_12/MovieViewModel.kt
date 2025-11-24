3ac41260d9131df5b85501837563c7e6bd1ef122
nadhiradanish <152785547+nadhiradanish@users.noreply.github.com>
2025-11-24 15:58:30 +0700

    Commit No. 2 – Upgrading app with flow

--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -5,25 +5,40 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.test_lab_week_12.model.Movie
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 
 class MovieViewModel(private val movieRepository: MovieRepository)
     : ViewModel() {
+
     init {
         fetchPopularMovies()
     }
-    // define the LiveData
-    val popularMovies: LiveData<List<Movie>>
-        get() = movieRepository.movies
-    val error: LiveData<String>
-        get() = movieRepository.error
+
+    private val _popularMovies = MutableStateFlow(emptyList<Movie>())
+    val popularMovies: StateFlow<List<Movie>> = _popularMovies
+
+    private val _error = MutableStateFlow("")
+    val error: StateFlow<String> = _error
+
     // fetch movies from the API
     private fun fetchPopularMovies() {
-// launch a coroutine in viewModelScope
-// Dispatchers.IO means that this coroutine will run on a shared
 
         viewModelScope.launch(Dispatchers.IO) {
+
             movieRepository.fetchMovies()
+                .catch { e ->
+                    // catch is a terminal operator that catches exceptions from the Flow
+                    _error.value = "An exception occurred: ${e.message}"
+                }
+                .collect { movies ->
+                    // collect is a terminal operator that collects the values from the Flow
+                    // the results are emitted to the StateFlow
+                    _popularMovies.value = movies
+                }
         }
     }
 }
